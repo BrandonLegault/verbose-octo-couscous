c6000d48938673bf69e6bfe740343a6fe675e12f
Andrew Fryer <andrew.fryer@queensu.ca>
2020-02-09 09:21:37 -0500

    test that player data is actually stored in model

--- main.php
+++ main.php
@@ -3,11 +3,15 @@
 include './PlayersViewModel.php';
 
 // I'm assuming I can refactor as much as I like, as long as this part still works
-$playersViewModel = new PlayersViewModel();
 
 // Test all 3 so that I know I haven't broken anything: (careful, they all print the same thing, so I need to make sure they don't just call the other test...)
-$playersViewModel->displayView(php_sapi_name() === 'cli', 'array');
-$playersViewModel->displayView(php_sapi_name() === 'cli', 'json');
-$playersViewModel->displayView(php_sapi_name() === 'cli', 'file', './playerdata.json');
+$vmFromArray = new PlayersViewModel($isCLI = php_sapi_name() === 'cli', $source = 'array');
+$vmFromArray->displayView();
+
+$vmFromJson = new PlayersViewModel($isCLI = php_sapi_name() === 'cli', $source = 'json');
+$vmFromJson->displayView();
+
+$vmFromFile = new PlayersViewModel($isCLI = php_sapi_name() === 'cli', $source = 'file', $filename = './playerdata.json');
+$vmFromFile->displayView();
 
 ?>
